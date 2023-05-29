021be2b2dadf769ee88bcf9c463e83115cdbc6e7
IsaacVGutierrez <90696341+IsaacVGutierrez@users.noreply.github.com>
2023-05-29 01:21:18 +0000

    Initial commit

---
+++ .build/index.js
@@ -0,0 +1,10 @@
+"use strict";
+let nombre;
+nombre = "Miguelo";
+let edad;
+edad = 30;
+const PERSONAJE = {
+  nombre,
+  edad
+};
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,9 @@
+
+let nombre: string; 
+nombre = "Miguelo"; 
+let edad: number; 
+edad = 30; 
+const PERSONAJE = { 
+nombre: nombre, 
+edad: edad 
+}; 
